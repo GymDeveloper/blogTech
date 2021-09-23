2c2e873317124fcc3d1a48b5d4f005e770753de6
linder3hs <linderhassinger00@gmail.com>
2021-09-23 11:12:37 -0500

    feat: add local and remote url mongo

--- config/config.js
+++ config/config.js
@@ -5,6 +5,8 @@ export const port = process.env.PORT || 8080;
 
 export const secret = process.env.SECRET || "secret";
 
-export const db_url =
-  process.env.DB_URL ||
+const mongo_local = "mongodb://192.168.1.4:27017/local";
+const mongo_remote =
   "mongodb+srv://root:6ixiU2NTTdQUOoY4@cluster0.wns0c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+export const db_url = process.env.DB_URL || mongo_local;
