140e2208b9614183e16384813c21e283d2ef841a
linder3hs <linderhassinger00@gmail.com>
2021-09-23 10:46:49 -0500

    feat: add user model

---
+++ api/components/user/model.js
@@ -0,0 +1,31 @@
+// este modelo sera el del usuario el cual contenga la estructura de datos
+// paso 1 importar mongoose
+import mongoose from "mongoose";
+// paso 2 definir los datos
+// Entendemos el usuario tiene
+/**
+ * id, name, last_name, email. password
+ */
+//* ojo por default mongo le pone un _id
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const userModel = mongoose.model("users", userSchema);
+
+export default userModel;
