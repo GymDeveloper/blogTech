468fc0868a22a36837bdb50a58b63b0aa9ca2b27
linder3hs <linderhassinger00@gmail.com>
2021-09-23 10:16:15 -0500

    feat: add db.js

---
+++ db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+//* tenemos que hacer que mongoose sea scope global
+mongoose.Promise = global.Promise;
+
+//* ahora debo crear una funcion la cual se encargue
+//* de conectar a mi base de datos
+//! como parametro tengo que recibir la URL
+
+/**
+ * Funcion para conectar a la base de datos
+ * @param {string} url
+ */
+const connect = async (url) => {
+  mongoose.connect(url, {
+    useNewUrlParser: true,
+  });
+};
+
+export default connect;
