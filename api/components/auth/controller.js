2fdb5d667099dba8a233b90b74165c774184810a
linder3hs <linderhassinger00@gmail.com>
2021-09-23 11:00:04 -0500

    feat: store with user model in auth

--- api/components/auth/controller.js
+++ api/components/auth/controller.js
@@ -1,7 +1,7 @@
-import { nanoid } from "nanoid";
 import { sign } from "../../../auth";
 import { store } from "../../../store/dummy";
 import { response } from "../../../network";
+import userModel from "../user/model";
 
 const USER_TABLE = "users";
 
@@ -30,15 +30,15 @@ export const signUp = async (req, res) => {
   const user = req.body;
 
   //* Creamos el data del usuario nuevo
+  // borramos el id porque mongodb ya lo pone
   const data = {
-    id: nanoid(),
     name: user.name,
     last_name: user.last_name,
     email: user.email,
     password: user.password,
   };
 
-  const users = await store(USER_TABLE, data);
+  const users = await store(userModel, data);
 
   return response({ res, data: users, status: 201 });
 };
